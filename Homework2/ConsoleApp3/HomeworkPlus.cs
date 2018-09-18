7743e2e8d58f5c98d8f46ec8c44e41ff0c7b1e94
Kurassier <43140751+Kurassier@users.noreply.github.com>
2018-09-19 00:43:33 +0800

    Add files via upload

---
+++ Homework2/ConsoleApp3/HomeworkPlus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework2
+{
+    class HomeworkPlus
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[12] {1,2,3,4,5,6,7,8,9,10,11,12};
+            int index = -1;
+            for(int i = 0; i < 11; i++)
+            {
+                for (int j = 1; j < 5; j++) 
+                {
+                    index++;
+                    if (index >= 12) index = 0;
+                    if (array[index] == 0) j--;
+                }
+                array[index] = 0;
+            }
+            while (array[index] == 0)
+            {
+                index++;
+                if (index >= 12) index = 0;
+            }
+            Console.WriteLine(array[index]);
+            Console.ReadLine();
+
+        }
+    }
+}
