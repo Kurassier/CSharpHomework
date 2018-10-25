89d763e49d3668d30202c42200fcddaff9eac261
Kurassier <43140751+Kurassier@users.noreply.github.com>
2018-10-25 22:38:53 +0800

    Add files via upload

---
+++ Homework6/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Homework4._2.test
+{
+    [TestClass]
+    public class OrderService
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.IsTrue(true);
+        }
+        public void DeleteTest()
+        {
+            Assert.IsTrue(true);
+        }
+        public void ChangeTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
+
